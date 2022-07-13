86db2fd1c2d0427ef525181bf3c4100af4e289e0
Paulo Fernando <paulofernandodcm16@gmail.com>
2022-07-12 21:37:04 -0300

    Other Components

---
+++ frontend/src/components/header/index.tsx
@@ -0,0 +1,18 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div id="dsmeta-logo-conteiner">
+                <img src={logo} alt="DSMeta"/>
+                    <h1>DSMeta</h1>
+                    <p>
+                        Desenvolvido por <a href="https://github.com/PauloFCM19" target="_blank">Paulo Fernando</a>
+                    </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
